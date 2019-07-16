d41d7c6ffffb0128708efda9b753d09786abec62
lyl123321 <2099367442@qq.com>
2019-07-16 17:48:06 +0800

    little

--- test.js
+++ test.js
@@ -1,385 +1,305 @@
 'use strict';
 //原型实现观察者模式
-function Events() {
-    this._listeners = {};
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? factory() :
+	typeof define === 'function' && define.amd ? define(factory) :
+	(factory());
+}(this, (function () { 'use strict';
+
+/**
+ * @this {Promise}
+ */
+function finallyConstructor(callback) {
+  var constructor = this.constructor;
+  return this.then(
+    function(value) {
+      // @ts-ignore
+      return constructor.resolve(callback()).then(function() {
+        return value;
+      });
+    },
+    function(reason) {
+      // @ts-ignore
+      return constructor.resolve(callback()).then(function() {
+        // @ts-ignore
+        return constructor.reject(reason);
+      });
+    }
+  );
 }
 
-Events.prototype = {
-    constructor: Events,
+// Store setTimeout reference so promise-polyfill will be unaffected by
+// other code modifying setTimeout (like sinon.useFakeTimers())
+var setTimeoutFunc = setTimeout;
 
-    addEvent: function(type, fn) {
-        this._listeners[type] = this._listeners[type] || [];
+function isArray(x) {
+  return Boolean(x && typeof x.length !== 'undefined');
+}
 
-        if (typeof fn === "function") {
-            this._listeners[type].push(fn);
-        }
+function noop() {}
 
-        return this;
-    },
+// Polyfill for Function.prototype.bind
+function bind(fn, thisArg) {
+  return function() {
+    fn.apply(thisArg, arguments);
+  };
+}
 
-    fireEvent: function(type) {
-        const eventArr = this._listeners[type];
+/**
+ * @constructor
+ * @param {Function} fn
+ */
+function Promise(fn) {
+  if (!(this instanceof Promise))
+    throw new TypeError('Promises must be constructed via new');
+  if (typeof fn !== 'function') throw new TypeError('not a function');
+  /** @type {!number} */
+  this._state = 0;
+  /** @type {!boolean} */
+  this._handled = false;
+  /** @type {Promise|undefined} */
+  this._value = undefined;
+  /** @type {!Array<!Function>} */
+  this._deferreds = [];
 
-        if (eventArr instanceof Array) {
-            for (let i = 0, len = eventArr.length; i < len; i++) {
-                if (typeof eventArr[i] === "function") {
-                    eventArr[i].call(this);
-                }
-            }
-        }
+  doResolve(fn, this);
+}
 
-        return this;
-    },
-    removeEvent: function(type, fn) {
-        const eventArr = this._listeners[type];
-
-        if (eventArr instanceof Array) {
-            if (typeof fn === "function") {
-                for (let i = 0, len = eventArr.length; i < len; i++) {
-                    if (eventArr[i] === fn) {
-                        eventArr.splice(i, 1);
-                        break;
-                    }
-                }
-            } else if (typeof fn === "undefined") {
-                delete this._listeners[type];
-            }
-        }
+function handle(self, deferred) {
+  while (self._state === 3) {
+    self = self._value;
+  }
+  if (self._state === 0) {
+    self._deferreds.push(deferred);
+    return;
+  }
+  self._handled = true;
+  Promise._immediateFn(function() {
+    var cb = self._state === 1 ? deferred.onFulfilled : deferred.onRejected;
+    if (cb === null) {
+      (self._state === 1 ? resolve : reject)(deferred.promise, self._value);
+      return;
     }
+    var ret;
+    try {
+      ret = cb(self._value);
+    } catch (e) {
+      reject(deferred.promise, e);
+      return;
+    }
+    resolve(deferred.promise, ret);
+  });
+}
+
+function resolve(self, newValue) {
+  try {
+    // Promise Resolution Procedure: https://github.com/promises-aplus/promises-spec#the-promise-resolution-procedure
+    if (newValue === self)
+      throw new TypeError('A promise cannot be resolved with itself.');
+    if (
+      newValue &&
+      (typeof newValue === 'object' || typeof newValue === 'function')
+    ) {
+      var then = newValue.then;
+      if (newValue instanceof Promise) {
+        self._state = 3;
+        self._value = newValue;
+        finale(self);
+        return;
+      } else if (typeof then === 'function') {
+        doResolve(bind(then, newValue), self);
+        return;
+      }
+    }
+    self._state = 1;
+    self._value = newValue;
+    finale(self);
+  } catch (e) {
+    reject(self, e);
+  }
+}
+
+function reject(self, newValue) {
+  self._state = 2;
+  self._value = newValue;
+  finale(self);
+}
+
+function finale(self) {
+  if (self._state === 2 && self._deferreds.length === 0) {
+    Promise._immediateFn(function() {
+      if (!self._handled) {
+        Promise._unhandledRejectionFn(self._value);
+      }
+    });
+  }
+
+  for (var i = 0, len = self._deferreds.length; i < len; i++) {
+    handle(self, self._deferreds[i]);
+  }
+  self._deferreds = null;
+}
+
+/**
+ * @constructor
+ */
+function Handler(onFulfilled, onRejected, promise) {
+  this.onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : null;
+  this.onRejected = typeof onRejected === 'function' ? onRejected : null;
+  this.promise = promise;
+}
+
+/**
+ * Take a potentially misbehaving resolver function and make sure
+ * onFulfilled and onRejected are only called once.
+ *
+ * Makes no guarantees about asynchrony.
+ */
+function doResolve(fn, self) {
+  var done = false;
+  try {
+    fn(
+      function(value) {
+        if (done) return;
+        done = true;
+        resolve(self, value);
+      },
+      function(reason) {
+        if (done) return;
+        done = true;
+        reject(self, reason);
+      }
+    );
+  } catch (ex) {
+    if (done) return;
+    done = true;
+    reject(self, ex);
+  }
+}
+
+Promise.prototype['catch'] = function(onRejected) {
+  return this.then(null, onRejected);
 };
-	
-;(function() {
-	function Promise(executor) {
-		const self = this;
-		
-		//存储状态的私有属性
-		this._status = 'pending';
-		
-		this.value = undefined;
-		this.reason = undefined;
-		this.events = new Events();
-		
-		//存储状态的公开属性
-		Object.defineProperty(this, 'status', {
-			get: function() {
-				return self._status;
-			},
-			set: function(newValue) {
-				self._status = newValue;
-				setTimeout(() => {
-					self.events.fireEvent('change');
-				});
-			},
-			configurable: true
-		});
-		
-		if(typeof executor === 'function') {
-			//捕获错误
-			try {
-				executor.call(null, function resolve(valueArg) {
-					innerResolve(self, valueArg);
-				}, function reject(reasonArg) {
-					self.reason = reasonArg;
-					self.status = 'rejected';
-				});
-			} catch(err) {
-				self.reason = err;
-				self.status = 'rejected';
-				setTimeout(() => {throw `(in promise) ${err}`});
-			}
-		}
-	}
-	
-	Object.defineProperties(Promise, {
-		resolve: {
-			value: resolve,
-			configurable: true,
-			writable: true
-		},
-		
-		reject: {
-			value: reject,
-			configurable: true,
-			writable: true
-		},
-		
-		try: {
-			value: tryFn,
-			configurable: true,
-			writable: true
-		},
-		
-		all: {
-			value: all,
-			configurable: true,
-			writable: true
-		},
-		
-		race: {
-			value: race,
-			configurable: true,
-			writable: true
-		}
-	});
-	
-	Promise.prototype = {
-		constructor: Promise,
-		
-		then: function(onFulfilled = identity, onRejected = thrower) {
-			const pro = new Promise();
-			const hander = () => {
-				let res;
-				try {
-					if(this.status === 'fulfilled') {
-						typeof onFulfilled !== 'function' ? onFulfilled = identity : false;
-						res = onFulfilled.call(null, this.value);
-					} else if(this.status === 'rejected') {
-						typeof onRejected !== 'function' ? onRejected = thrower : false;
-						res = onRejected.call(null, this.reason);
-					}
-				} catch(err) {
-					pro.reason = err;
-					pro.status = 'rejected';
-					return;
-				}
-				innerResolve(pro, res);
-			}
-			this.events.addEvent('change', hander);
-			return pro;
-		},
-		
-		catch: function(onRejected) {
-			return this.then(undefined, onRejected);
-		},
-		
-		finally: function(onFinally) {
-			const pro = new Promise();
-			const hander = () => {
-				let res;
-				try {
-					res = typeof onFinally === 'function' ? onFinally() : undefined;
-				} catch(err) {
-					pro.reason = err;
-					pro.status = 'rejected';
-					return;
-				}
-				if(res instanceof Promise && res.status === 'rejected') {
-					pro.reason = res.reason;
-					pro.status = 'rejected';
-				} else if(res instanceof Promise && res.status === 'pending') {
-					follow(pro, res);
-				} else {
-					pro.value = this.value;
-					pro.reason = this.reason;
-					pro.status = this.status;
-				}
-			}
-			this.events.addEvent('change', hander);
-			return pro;
-		},
-	}
-	
-	function resolve(value) {
-		if(value instanceof Promise) {
-			return value;
-		}
-		
-		//thenable 对象
-		if(typeof value === 'object' && typeof value.then === 'function') {
-			return new Promise(value.then);
-		}
-		
-		const pro = new Promise();
-		
-		pro.value = value;
-		pro.status = 'fulfilled';
-		
-		return pro;
-	}
-	
-	function reject(reason) {
-		const pro = new Promise();
-		
-		pro.reason = reason;
-		pro.status = 'rejected';
-		
-		return pro;
-	}
-	
-	//实现 Promise.try, 方便处理一个函数
-	function tryFn(fn, thisArg = null, ...args) {
-		if(typeof fn === 'function') {
-			return new Promise(resolve => resolve(fn.apply(thisArg, args)));
-		} else {
-			const err = new TypeError(`${typeof fn} ${fn} is not a function`);
-			return Promise.try(() => {throw err});
-		}
-	}
-	
-	function all(iterable) {
-		if(iterable[Symbol.iterator] === undefined) {
-			const err = new TypeError(`${typeof iterable} ${iterable} is not iterable (cannot read property Symbol(Symbol.iterator))`);
-			return Promise.try(() => {throw err});
-		}
-		
-		const iteArray = Array.from(iterable);
-		const len = iteArray.length;
-		
-		if(len === 0) {
-			return Promise.resolve([]);
-		}
-		
-		const pro = new Promise();
-		const valueArr = [];
-		
-		setTimeout(function() {
-			let index = 0,	//记录当前索引
-				count = 0;
-			
-			for(let val of iteArray) {
-				-function(i){
-					if(val instanceof Promise) {		//当前值为 Promise 对象时
-						if(val.status === 'pending') {
-							val.then(function(value) {
-								valueArr[i] = value;
-								count++;
-								
-								if(count === len) {
-									pro.value = valueArr;
-									pro.status = 'fulfilled';
-								}
-							}, function(reason) {
-								pro.reason = reason;
-								pro.status = 'rejected';
-								removeEv(iteArray);
-							});
-						} else if(val.status === 'rejected') {
-							pro.reason = val.reason;
-							pro.status = 'rejected';
-							return;
-						} else {
-							valueArr[i] = val.value;
-							count++;
-						}
-					} else {
-						valueArr[i] = val;
-						count++;
-					}
-					
-					index++;
-				}(index);
-			}
-			
-			if(count === len) {
-				pro.value = valueArr;
-				pro.status = 'fulfilled';
-			}
-		});
-		
-		return pro;
-	}
-	
-	function race(iterable) {
-		if(iterable[Symbol.iterator] === undefined) {
-			const err = new TypeError(`${typeof iterable} ${iterable} is not iterable (cannot read property Symbol(Symbol.iterator))`);
-			return Promise.try(() => {throw err});
-		}
-		
-		const iteArray = Array.from(iterable);
-		const len = iteArray.length;
-		
-		if(len === 0) {
-			return Promise.resolve([]);
-		}
-		
-		const pro = new Promise();
-		
-		setTimeout(function() {
-			for(let val of iteArray) {
-				if(val instanceof Promise) {
-					if(val.status === 'pending') {
-						val.then(function(value) {
-							pro.value = value;
-							pro.status = 'fulfilled';
-							removeEv(iteArray);
-						}, function(reason) {
-							pro.reason = reason;
-							pro.status = 'rejected';
-							removeEv(iteArray);
-						});
-					} else if(val.status === 'rejected') {
-						pro.reason = val.reason;
-						pro.status = 'rejected';
-						removeEv(iteArray);
-						return;
-					} else {
-						pro.value = val.value;
-						pro.status = 'fulfilled';
-						removeEv(iteArray);
-						return;
-					}
-				} else {
-					pro.value = val;
-					pro.status = 'fulfilled';
-					removeEv(iteArray);
-					return;
-				}
-			}
-		});
-		
-		return pro;
-	}
-	
-	function identity(value) {
-		return value;
-	}
-	
-	function thrower(reason) {
-		throw reason;
-	}
-	
-	function removeEv(iterable) {
-		for(let uselessPromise of iterable) {
-			if(uselessPromise instanceof Promise && uselessPromise.status === 'pending') {
-				uselessPromise.events.removeEvent('change');
-			}
-		}
-	}
-	
-	function follow(a, b) {
-		b.then(function(value) {
-			a.value = value;
-			a.status = 'fulfilled';
-		}, function(reason) {
-			a.reason = reason;
-			a.status = 'rejected';
-		});
-	}
-	
-	function innerResolve(self, valueArg) {
-		if(valueArg instanceof Promise) {
-			if(valueArg.status === 'fulfilled') {
-				self.value = valueArg.value;
-				self.status = 'fulfilled';
-			} else if(valueArg.status === 'rejected') {
-				self.reason = valueArg.reason;
-				self.status = 'rejected';
-			} else {
-				follow(self, valueArg)
-			}
-		} else {
-			self.value = valueArg;
-			self.status = 'fulfilled';
-		}
-	}
-	
-	window._Promise = Promise;
+
+Promise.prototype.then = function(onFulfilled, onRejected) {
+  // @ts-ignore
+  var prom = new this.constructor(noop);
+
+  handle(this, new Handler(onFulfilled, onRejected, prom));
+  return prom;
+};
+
+Promise.prototype['finally'] = finallyConstructor;
+
+Promise.all = function(arr) {
+  return new Promise(function(resolve, reject) {
+    if (!isArray(arr)) {
+      return reject(new TypeError('Promise.all accepts an array'));
+    }
+
+    var args = Array.prototype.slice.call(arr);
+    if (args.length === 0) return resolve([]);
+    var remaining = args.length;
+
+    function res(i, val) {
+      try {
+        if (val && (typeof val === 'object' || typeof val === 'function')) {
+          var then = val.then;
+          if (typeof then === 'function') {
+            then.call(
+              val,
+              function(val) {
+                res(i, val);
+              },
+              reject
+            );
+            return;
+          }
+        }
+        args[i] = val;
+        if (--remaining === 0) {
+          resolve(args);
+        }
+      } catch (ex) {
+        reject(ex);
+      }
+    }
+
+    for (var i = 0; i < args.length; i++) {
+      res(i, args[i]);
+    }
+  });
+};
+
+Promise.resolve = function(value) {
+  if (value && typeof value === 'object' && value.constructor === Promise) {
+    return value;
+  }
+
+  return new Promise(function(resolve) {
+    resolve(value);
+  });
+};
+
+Promise.reject = function(value) {
+  return new Promise(function(resolve, reject) {
+    reject(value);
+  });
+};
+
+Promise.race = function(arr) {
+  return new Promise(function(resolve, reject) {
+    if (!isArray(arr)) {
+      return reject(new TypeError('Promise.race accepts an array'));
+    }
+
+    for (var i = 0, len = arr.length; i < len; i++) {
+      Promise.resolve(arr[i]).then(resolve, reject);
+    }
+  });
+};
+
+// Use polyfill for setImmediate for performance gains
+Promise._immediateFn =
+  // @ts-ignore
+  (typeof setImmediate === 'function' &&
+    function(fn) {
+      // @ts-ignore
+      setImmediate(fn);
+    }) ||
+  function(fn) {
+    setTimeoutFunc(fn, 0);
+  };
+
+Promise._unhandledRejectionFn = function _unhandledRejectionFn(err) {
+  if (typeof console !== 'undefined' && console) {
+    console.warn('Possible Unhandled Promise Rejection:', err); // eslint-disable-line no-console
+  }
+};
+
+/** @suppress {undefinedVars} */
+var globalNS = (function() {
+  // the only reliable means to get the global object is
+  // `Function('return this')()`
+  // However, this causes CSP violations in Chrome apps.
+  if (typeof self !== 'undefined') {
+    return self;
+  }
+  if (typeof window !== 'undefined') {
+    return window;
+  }
+  if (typeof global !== 'undefined') {
+    return global;
+  }
+  throw new Error('unable to locate global object');
 })();
 
-var p1 = new Promise((re, rj) => {
-	rj(1);
-}).catch(d => console.log(d));
+if (!('Promise' in globalNS)) {
+  globalNS['Promise'] = Promise;
+} else if (!globalNS.Promise.prototype['finally']) {
+  globalNS.Promise.prototype['finally'] = finallyConstructor;
+}
 
-var p2 = new _Promise((re, rj) => {
-	rj(2);
-}).catch(d => console.log(d));
+})));
+
+console.log(Promise.race([1, 2, new Promise((re, rj) => {setTimeout(rj, 500)})]));
